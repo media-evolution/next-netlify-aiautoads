f6d63eb5e1d8cce2e4ba6b5c8b6ad997fa1e5964
Spencer Toogood <95565967+media-evolution@users.noreply.github.com>
2024-09-23 15:01:42 +1000

    Initial Test Commit

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Ai, Automation & Ads" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          With Spencer
         </p>
       </main>
 
